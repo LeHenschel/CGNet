e89950c12a2bdb8d7e1c10924b9dc5db7890c2c9
LeHenschel <leonie.henschel@dzne.de>
2021-07-13 13:34:16 +0200

    Added some comments and flag to disable data download

--- MNIST/MNIST_data_process.py
+++ MNIST/MNIST_data_process.py
@@ -1,7 +1,6 @@
 from importlib import reload
 import numpy as np
 import os
-import sys
 import utils.geometries as geometries; reload(geometries)
 import gzip
 import logging
@@ -21,7 +20,8 @@ _CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 
 def download_file(url, dst, overwrite=False):
     # NOTE the stream=True parameter below
-    if not overwrite: assert not os.path.isfile(dst), "{} already exists.".format(dst)
+    if not overwrite:
+        assert not os.path.isfile(dst), "{} already exists.".format(dst)
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with open(dst, 'wb') as f:
@@ -34,9 +34,9 @@ def download_file(url, dst, overwrite=False):
     return dst
 
 
-
-def download(data_dir = None):
-    if data_dir is None: data_dir = _settings.MNIST_PATH
+def download(data_dir=None):
+    if data_dir is None:
+        data_dir = _settings.MNIST_PATH
     if not os.path.isdir(data_dir):
         os.makedirs(data_dir)
     filenames = ["train-images-idx3-ubyte.gz", "train-labels-idx1-ubyte.gz", "t10k-images-idx3-ubyte.gz",
@@ -93,12 +93,14 @@ def _get_input_from2D(img2D, lmax=14, rotate_angles=None, separateParts=False, r
     else:
         return coefs, hs
 
+
 def np_save_safe(file_path, data):
     folder_path = os.path.dirname(file_path)
     if not os.path.isdir(folder_path):
         os.makedirs(folder_path)
     print("Saving to {}".format(file_path))
     np.save(file_path, data)
+
 
 def get_inputs_from2D(lmax, img2Ds, random_rotate=False, rot_func=False, a=1.0, amax=None):
     logger = logging.getLogger("_datautils_logger")
@@ -108,7 +110,8 @@ def get_inputs_from2D(lmax, img2Ds, random_rotate=False, rot_func=False, a=1.0, 
     assert len(img2Ds.shape) == 3, "(index, x, y)"
     batch_size = len(img2Ds)
     fs = []
-    if amax is not None: amin = a
+    if amax is not None:
+        amin = a
     for i in tqdm.tqdm(range(batch_size), ncols=60):
         angles = 2 * np.pi * np.random.uniform(size=3) if random_rotate else None
         if i % 100 == 0 and logger is not None:
@@ -119,45 +122,66 @@ def get_inputs_from2D(lmax, img2Ds, random_rotate=False, rot_func=False, a=1.0, 
     f_0 = np.stack(fs, 0)
     return f_0
 
+
 def __find_old_coefs(lmax, fdir):
     fs = os.listdir(fdir)
-    if len(fs) == 0: return None
+    if len(fs) == 0:
+        return None
     old_lmaxs = [int(f.split("_")[1].split(".")[0]) for f in fs]
     if max(old_lmaxs) >= lmax:
         return os.path.join(fdir, 'L_%d.npy'%max(old_lmaxs))
     return None
 
-def precomputing_coefs_new(lmax, split='test', rotate=False, data_dir=None, recompute=False, rot_func=False, a=1.0, amax=None):
-    data_dir, filenames = download(data_dir)
+
+def precomputing_coefs_new(lmax, split='test', rotate=False, data_dir=_settings.MNIST_PATH, recompute=False,
+                           data_download=False, rot_func=False, a=1.0, amax=None):
+
+    # Download and extract data
+    if data_download:
+        data_dir, filenames = download(data_dir)
+
+    # Define directories with training/testing data
     coefs_folder = os.path.join(data_dir, 'coefs')
     coeftype = f"{split}{'_rotate' if rotate else ''}"
-    if rotate and rot_func: coeftype += "_rotfunc"
+    if rotate and rot_func:
+        coeftype += "_rotfunc"
     if a != 1.0:
         a = np.round(a, 2)
         coeftype += f"_a{a:.2f}"
         if amax is not None:
             coeftype += f"-{amax:.2f}"
     file_path = os.path.join(coefs_folder, coeftype, f"L_{lmax}.npy")
+
+    # Load precomputed train/test data
     if not recompute:
-        if not os.path.isdir(os.path.dirname(file_path)): os.makedirs(os.path.dirname(file_path))
+        if not os.path.isdir(os.path.dirname(file_path)):
+            os.makedirs(os.path.dirname(file_path))
         old_fpath = __find_old_coefs(lmax, os.path.dirname(file_path))
         if old_fpath is not None:
-            print('Loading lmax=%d from %s'%(lmax, old_fpath))
-            return np.load(old_fpath)[:,:lmax**2]
+            print('Loading lmax=%d from %s' % (lmax, old_fpath))
+            return np.load(old_fpath)[:, :lmax**2]
+
+    # or recalculate coefficients
     data_file_name = os.path.join(data_dir, filenames[{'train': 0, 'test': 2}[split]])
     data = extract_data(data_file_name, 60000 if split == 'train' else 10000)
     coefs = get_inputs_from2D(lmax, data, random_rotate=rotate, rot_func=rot_func, a=a, amax=amax)
+
+    # save to file
     np_save_safe(file_path, coefs)
     return coefs, data
+
 
 def get_labels(split='test', data_dir=None):
     data_dir, filenames = download(data_dir)
     data_file_name = os.path.join(data_dir, filenames[{'train': 1, 'test': 3}[split]])
-    cache_file_path = os.path.join(data_dir, "label_%s.npy"%split)
-    if os.path.isfile(cache_file_path): return np.load(cache_file_path)
-    labels = extract_labels(data_file_name, 60000 if split == 'train' else 10000)
-    np_save_safe(cache_file_path, labels)
+    cache_file_path = os.path.join(data_dir, "label_%s.npy" % split)
+    if os.path.isfile(cache_file_path):
+        labels = np.load(cache_file_path)
+    else:
+        labels = extract_labels(data_file_name, 60000 if split == 'train' else 10000)
+        np_save_safe(cache_file_path, labels)
     return labels
+
 
 if __name__ == "__main__":
     for lmax in [12]: #scale
