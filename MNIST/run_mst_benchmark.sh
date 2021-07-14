ca763e65c1aaa7aaa96ae02555f06a9177a7db72
Oscar Carlsson <osccarls@chalmers.se>
2021-07-14 19:28:12 +0200

    Updated run script for benchmark

--- MNIST/run_mst_benchmark.sh
+++ MNIST/run_mst_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a mst_weight_type=(cost random sum none)
+declare -a mst_weight_type=("" "--mst --mst_weight cost" "--mst --mst_weight random" "--mst --mst_weight sum" "--mst --mst_weight none")
 num_iterations=5
 
 path=$(pwd)
@@ -8,13 +8,13 @@ main_script=$path/main.py
 
 csv_file=$path/benchmark.csv
 
-for mst_weight in "${mst_weight_type[@]}"; do
-    for ((iteration=0; iteration<$num_iterations; iteration++)); do
-         bash ./launch_mnist_run.sh "$main_script" --mst "--mst_weight $mst_weight" "--csv_file $csv_file"  # For HPC change bash to sbatch
+for ((iteration=0; iteration<$num_iterations; iteration++)); do
+    for mst_weight in "${mst_weight_type[@]}"; do
+         echo bash ./launch_mnist_run.sh "$main_script" "$mst_weight" "--csv_file $csv_file"  # For HPC change bash to sbatch
     done
 done
 
 
-for ((iteration=0; iteration<$num_iterations; iteration++)); do
-     bash ./launch_mnist_run.sh "$main_script" "--csv_file $csv_file"
-done
+# for ((iteration=0; iteration<$num_iterations; iteration++)); do
+#      bash ./launch_mnist_run.sh "$main_script" "--csv_file $csv_file"
+# done
