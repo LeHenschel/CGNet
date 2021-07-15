f84a26399400ac32caef48844c72ab34ea1c7121
LeHenschel <leonie.henschel@dzne.de>
2021-07-15 10:57:48 +0200

    Adopted run-scripts to run in Colab

--- MNIST/run_mst_benchmark.sh
+++ MNIST/run_mst_benchmark.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 declare -a mst_weight_type=("" "--mst --mst_weight cost" "--mst --mst_weight random" "--mst --mst_weight sum" "--mst --mst_weight none")
-num_iterations=5
+num_iterations=1
 
 path=$(pwd)
-main_script=$path/main.py
+main_script=$path/MNIST/main.py
 
-csv_file=$path/benchmark.csv
+csv_file=$path/MNIST/benchmark.csv
 
 for ((iteration=0; iteration<$num_iterations; iteration++)); do
     for mst_weight in "${mst_weight_type[@]}"; do
-         echo bash ./launch_mnist_run.sh "$main_script" "$mst_weight" "--csv_file $csv_file"  # For HPC change bash to sbatch
+         bash ./MNIST/launch_mnist_run.sh "$main_script" "$mst_weight" "--csv_file $csv_file"  # For HPC change bash to sbatch
     done
 done
 
