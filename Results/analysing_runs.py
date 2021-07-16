0e50d5dc196391101b8cbfbdb145bcc512b0afff
LeHenschel <leonie.henschel@dzne.de>
2021-07-16 16:10:14 +0200

    Updated analysis script for final image generation

--- Results/analysing_runs.py
+++ Results/analysing_runs.py
@@ -17,120 +17,45 @@ def convert_str_to_bool(arr):
 
     return np.array(new)
 
-mst_used = []
-mst_weight = []
-run_time = []
-best_error = []
 
-with open('benchmark.csv') as csvDataFile:
-    csvReader = csv.reader(csvDataFile)
-    for row in csvReader:
-        mst_used.append(row[0])
-        mst_weight.append(row[1])
-        run_time.append(row[2])
-        best_error.append(row[3])
+if __name__ == "__main__":
+    import pandas as pd
+    import seaborn as sns
+    import matplotlib as mpl
+    plt.style.use("seaborn")
+    df = pd.read_csv("benchmark.csv")
+    df['Type'] = ["+ " + x if y is True else "- MST" for (x, y) in zip(df["mst_weight"], df['mst'])]
+    df['best_error'] = df["best_error"] * 100
+    mean_none_mst = df["train_time"].loc[(df["Type"] == "- MST")].mean()
+    df["train_time"] = df["train_time"] / mean_none_mst
 
-mst_used = np.array(mst_used[1:])
-mst_used = convert_str_to_bool(mst_used)
-mst_weight = np.array(mst_weight[1:])
-run_time = np.array(run_time[1:])
-best_error = np.array(best_error[1:])
+    prop = mpl.font_manager.FontProperties(family='sans-serif', size=6, weight="bold", style="normal")
+    tick_prop = mpl.font_manager.FontProperties(family='sans-serif', size=6, weight="normal", style="normal")
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    ax1 = sns.boxplot(y="train_time", x="Type", ax=ax[0], linewidth=1.0, data=df)
+    ax1.grid(b=True)
+    ax1.set_xlabel("", fontproperties=prop)
+    ax1.set_ylabel("Rel. Training time", fontproperties=prop)
+    for label in ax1.get_xticklabels():
+        label.set_fontproperties(tick_prop)
 
-no_mst_run_time = np.array([int(time) for time, used in zip(run_time, mst_used) if not used])
-no_mst_err = np.array([float(berr) for berr, used in zip(best_error, mst_used) if not used])
+    for tick in ax1.get_xticklabels():
+        tick.set_rotation(90)
 
-mst_run_time = np.array([time for time, used in zip(run_time, mst_used) if used])
-mst_best_error = np.array([berr for berr, used in zip(best_error, mst_used) if used])
-mst_weight = np.array([weight for weight, used in zip(mst_weight, mst_used) if used])
+    ax2 = sns.boxplot(y="best_error", x="Type", ax=ax[1], linewidth=1.0, data=df)
+    ax2.grid(b=True)
+    ax2.set_xlabel("", fontproperties=prop)
+    ax2.set_ylabel("Best Error [% FP]", fontproperties=prop)
+    for label in ax2.get_xticklabels():
+        label.set_fontproperties(tick_prop)
 
-sort_idx = np.argsort(mst_weight)
+    for tick in ax2.get_xticklabels():
+        tick.set_rotation(90)
+    plt.subplots_adjust(wspace=.05)
 
-mst_weight = mst_weight[sort_idx]
+    fig.savefig("Test.svg")
+    axis_width = "\\figw"
+    axis_height = "\\figh"
+    tpl.save("data_pd.tex", axis_width=axis_width, axis_height=axis_height)
 
-mst_used = mst_used[sort_idx]
-mst_run_time = mst_run_time[sort_idx]
-mst_best_error = mst_best_error[sort_idx]
-
-
-cost_run_time = [int(time) for time, used in zip(mst_run_time, mst_weight) if used=="cost"]
-cost_err = [float(berr) for berr, used in zip(mst_best_error, mst_weight) if used=="cost"]
-
-random_run_time = [int(time) for time, used in zip(mst_run_time, mst_weight) if used=="random"]
-random_err = [float(berr) for berr, used in zip(mst_best_error, mst_weight) if used=="random"]
-
-sum_run_time = [int(time) for time, used in zip(mst_run_time, mst_weight) if used=="sum"]
-sum_err = [float(berr) for berr, used in zip(mst_best_error, mst_weight) if used=="sum"]
-
-none_run_time = [int(time) for time, used in zip(mst_run_time, mst_weight) if used=="none"]
-none_err = [float(berr) for berr, used in zip(mst_best_error, mst_weight) if used=="none"]
-
-mean_no_mst_run_time = np.mean(no_mst_run_time)
-std_no_mst_run_time = np.std(no_mst_run_time)
-
-mean_cost_run_time = np.mean(cost_run_time)
-std_cost_run_time = np.std(cost_run_time)
-
-mean_random_run_time = np.mean(random_run_time)
-std_random_run_time = np.std(random_run_time)
-
-mean_sum_run_time = np.mean(sum_run_time)
-std_sum_run_time = np.std(sum_run_time)
-
-mean_none_run_time = np.mean(none_run_time)
-std_none_run_time = np.std(none_run_time)
-
-mean_no_mst_err = np.mean(no_mst_err)
-std_no_mst_err = np.std(no_mst_err)
-
-mean_cost_err = np.mean(cost_err)
-std_cost_err = np.std(cost_err)
-
-mean_random_err = np.mean(random_err)
-std_random_err = np.std(random_err)
-
-mean_sum_err = np.mean(sum_err)
-std_sum_err = np.std(sum_err)
-
-mean_none_err = np.mean(none_err)
-std_none_err = np.std(none_err)
-
-
-ms = 1.5
-es = 3
-fmt = "o"
-
-_, ax = plt.subplots(2,1, sharex=True)
-ax[0].errorbar(1,mean_no_mst_run_time, std_no_mst_run_time, fmt=fmt, capsize=es, ms=ms)
-ax[0].errorbar(2,mean_cost_run_time, std_cost_run_time, fmt=fmt, capsize=es, ms=ms)
-ax[0].errorbar(3,mean_sum_run_time, std_sum_run_time, fmt=fmt, capsize=es, ms=ms)
-ax[0].errorbar(4,mean_none_run_time, std_none_run_time, fmt=fmt, capsize=es, ms=ms)
-ax[0].errorbar(5,mean_random_run_time, std_random_run_time, fmt=fmt, capsize=es, ms=ms)
-
-ylim = list(ax[0].get_ylim())
-ylim[0]=0
-ax[0].set_ylim(ylim)
-
-ax[0].yaxis.grid(True, which='major')
-ax[0].yaxis.grid(True, which='minor')
-ax[0].set_title("Run time [s]")
-ax[0].set_ylabel("[s]")
-
-ax[1].errorbar(1,mean_no_mst_err, std_no_mst_err, fmt=fmt, capsize=es, ms=ms)
-ax[1].errorbar(2,mean_cost_err, std_cost_err, fmt=fmt, capsize=es, ms=ms)
-ax[1].errorbar(3,mean_sum_err, std_sum_err, fmt=fmt, capsize=es, ms=ms)
-ax[1].errorbar(4,mean_none_err, std_none_err, fmt=fmt, capsize=es, ms=ms)
-ax[1].errorbar(5,mean_random_err, std_random_err, fmt=fmt, capsize=es, ms=ms)
-
-ylim = list(ax[1].get_ylim())
-ylim[0]=0
-ax[1].set_ylim(ylim)
-
-ax[1].yaxis.grid(True, which='major')
-ax[1].yaxis.grid(True, which='minor')
-ax[1].set_title("Best Error")
-# plt.show()
-
-axis_width = "\\fig_width"
-axis_height = "\\fig_height"
-
-tpl.save("data.tex", axis_width=axis_width, axis_height=axis_height)
+    plt.close(fig)
